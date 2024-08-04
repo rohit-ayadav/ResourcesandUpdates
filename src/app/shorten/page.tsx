a4ed22839383a9dfca1b1db63460bb25585292e3
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-08-04 12:11:06 +0530

    Delete src/app/shorten directory

--- src/app/shorten/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/shorten/page.tsx
-export default function ShortenPage() {
-    return (
-        <div>
-            <h1>Welcome to the URL Shortener</h1>
-            <p>This is a static page. The URL shortening functionality will be added later.</p>
-        </div>
-    );
-}
