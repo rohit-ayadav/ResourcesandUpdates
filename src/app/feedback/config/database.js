3fb1f79324f10a5250b43ef9d63b5eea207046e9
Rohit kumar yadav <rohitkuyada@gmail.com>
2024-08-14 21:34:17 +0530

    chore: Remove unused feedback API endpoint and database configuration

--- src/app/feedback/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('your_database_name', 'your_username', 'your_password', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
-
-module.exports = sequelize;
